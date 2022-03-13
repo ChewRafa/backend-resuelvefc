d4c00349c12ee6ac9f7b40b4223c81419c1958b7
Rafael Ramírez Ocaña <yphar89@gmail.com>
2022-03-12 21:24:56 -0600

    añadir validacion a helper, trabajar en local

--- helpers.php
+++ helpers.php
@@ -2,9 +2,25 @@
 
 //determinar alcance individual
 function determinarAlcanceIndividual($golesMes, $minimoGoles) {
+
+    if (isset($golesMes) && isset($minimoGoles)) {
         return $golesMes / $minimoGoles;
+    } else {
+        return false;
+    }
 }
 
 function determinarAlcanceEquipo($totalGoles = [], $minimoRequerido = []) {
+    if (isset($totalGoles) && isset($minimoRequerido)) {
         return array_sum($totalGoles) / array_sum($minimoRequerido);
+    } else {
+       return false;
+   }
 }
+// calcular el alcance  del jugador de acuerdo al desempeño
+function calcularAlcanceTotal($alcanceIndividual, $alcanceEquipo) {
+    if (isset($alcanceIndividual) && isset($alcanceEquipo)) {
+        return ($alcanceIndividual + $alcanceEquipo) / 2;
+    }
+}
+
--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $data = <<<EOD
 {"jugadores":[{"nombre":"Juan Perez","nivel":"C","goles":10,"sueldo":50000,"bono":25000,"sueldo_completo":null,"equipo":"rojo"},{"nombre":"EL Cuauh","nivel":"Cuauh","goles":30,"sueldo":100000,"bono":30000,"sueldo_completo":null,"equipo":"azul"},{"nombre":"Cosme Fulanito","nivel":"A","goles":7,"sueldo":20000,"bono":10000,"sueldo_completo":null,"equipo":"azul"},{"nombre":"El Rulo","nivel":"B","goles":9,"sueldo":30000,"bono":15000,"sueldo_completo":null,"equipo":"rojo"}]}
 EOD;
 
+if ($data) {
 
     $jsonData = json_decode($data);
 
@@ -17,7 +18,12 @@ EOD;
     foreach ($jsonData as $key => $value) {
         foreach ($value as $key2 => $value2) {
             $equipos
-            $equipos[$value[$key2]->equipo];
+//            $equipos[$value[$key2]->equipo];
             
         }
     }
+    
+} else {
+    http_response_code(400);
+    print("Petición Vacía");
+}
